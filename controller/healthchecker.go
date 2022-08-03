364570ab675f4ac4fe6e1fbc4b07e50b824110d9
Yuki Maruyama <dev@32ba.net>
2022-08-04 01:36:44 +0900

    add healthchecker

---
+++ controller/healthchecker.go
@@ -0,0 +1,13 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func HealthChecker(c *gin.Context){
+	c.JSON(http.StatusCreated, gin.H{
+        "status": "ok",
+    })
+}
--- main.go
+++ main.go
@@ -1,5 +1,15 @@
-package main 
+package main
 
-func main(){
-		
+import (
+	"go-ranking-api/controller"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	engine := gin.Default()
+
+	engine.GET("/healthchecker", controller.HealthChecker)
+
+	engine.Run(":8080")
 }
